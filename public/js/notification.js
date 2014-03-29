57fdb1066720831f4cee94aa6cbb053123163576
kevinhill@live.com <kevinhill@live.com>
2014-03-29 11:36:01 -0700

    adding bridge

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,13 +1,7 @@
 
-document.addEventListener('deviceready', function(){
-
-var newButton = document.getElementById('resetTimer');
-
-newButton.addEventListener('click', function(){
-
-eventBecon.src = 'beacon.html?EVENT=NOTIFICATIONEVENT='+document.getElementById('timer').innerText;
-
-})
-
-
-})
+document.addEventListener('deviceready', function() {
+		var newButton = document.getElementById('resetTimer');
+		newButton.addEventListener('click', function(){
+		eventBecon.src = 'beacon.html?EVENT=NOTIFICATIONEVENT='+document.getElementById('timer').innerText;
+	});
+});
