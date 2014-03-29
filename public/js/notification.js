91c0bd2c54d84b5f8ae68b2443b4439cc222bed6
boyofgreen <boyofgreen@gmail.com>
2014-03-28 22:39:22 -0700

    add files for store project

---
+++ public/js/notification.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('deviceready', function(){
+
+var newButton = document.getElementById('resetTimer');
+
+newButton.addEventListener('click', function(){
+
+eventBecon.src = 'beacon.html?EVENT=NOTIFICATIONEVENT='+document.getElementById('timer').innerText;
+
+})
+
+
+})
